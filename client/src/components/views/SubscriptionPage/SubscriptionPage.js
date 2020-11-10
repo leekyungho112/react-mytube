16eac715aedb31931f0804d45501658770e7b7eb
leekyungho112 <epsoe@naver.com>
2020-11-10 11:02:55 +0900

    video upload

--- client/src/components/views/SubscriptionPage/SubscriptionPage.js
+++ client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -60,7 +60,7 @@ function SubscriptionPage() {
 
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
-      <Title level={2}>Recommended</Title>
+      <Title level={2}>구독페이지</Title>
       <hr />
       <Row gutter={[32, 16]}>{renderCards}</Row>
     </div>
